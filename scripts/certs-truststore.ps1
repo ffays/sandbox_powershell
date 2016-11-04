66003ca8a1ea45ff8e2a5eeca85b66c2334d24e1
Frederic Fays <frederic@fays.eu>
2016-11-04 16:54:57 +0100

    Added a listing of the trusted certificates

---
+++ scripts/certs-truststore.ps1
@@ -0,0 +1,5 @@
+﻿ls Cert:\CurrentUser\Root | ? {$_.SignatureAlgorithm.FriendlyName -eq "sha256RSA" } | `
+    ft @{Name="NotBefore";Expression={$_.NotBefore};FormatString="dd-MMM-yyyy"}`
+      ,@{Name="NotAfter";Expression={$_.NotAfter};FormatString="dd-MMM-yyyy"}`
+      ,@{Name="SignatureAlgorithm";Expression={$_.SignatureAlgorithm.FriendlyName}}`
+      ,Subject -AutoSize
