cd82ea6dbd471ce170682b4e1c74aec0dd74a881
Frederic Fays <frederic@fays.eu>
2017-04-25 18:02:37 +0200

    Added scriplet

---
+++ scripts/eclipse-find-feature-having-plugin.ps1
@@ -0,0 +1,17 @@
+﻿$eclipseHome = "${env:USERPROFILE}\eclipse\rcp-oxygen\eclipse"
+
+$lookupValue = 'org.eclipse.jdt'
+
+Get-ChildItem -Recurse "${eclipseHome}\features" ` | Where-Object Name -eq 'feature.xml' `
+| % {
+    $xml = [xml](Get-Content $_.FullName) 
+    $featureId = $xml.SelectSingleNode("/feature/@id").value
+    $featureFile = $_.FullName
+    $plugins = $xml.SelectNodes("/feature/plugin")
+    $pluginIds = $plugins | % {$_.id}
+    
+    if($pluginIds -contains $lookupValue) {
+        Write-Output "${featureId} ${featureFile}"
+        #Write-Output $plugins
+    }
+}
