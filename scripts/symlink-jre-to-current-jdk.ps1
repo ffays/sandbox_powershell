36daa212a54860f07c4472908c2f13a4c3d1c3aa
Frederic Fays <frederic@fays.eu>
2017-06-07 15:29:26 +0200

    First version

---
+++ scripts/symlink-jre-to-current-jdk.ps1
@@ -0,0 +1,14 @@
+﻿# Execute the next line once from an interactive Powershell session
+# Set-ExecutionPolicy RemoteSigned CurrentUser -Force
+
+# Creates a symbolic link "jre" in the current folder to the current JDK
+$jdkRegistryPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Development Kit'
+$jdkVersion = Get-ItemPropertyValue -Path $jdkRegistryPath -Name CurrentVersion
+$javaHome = Get-ItemPropertyValue -Path "${jdkRegistryPath}\$jdkVersion" -Name JavaHome
+$jreFolderName = "jre"
+
+If($(Test-Path $jreFolderName)) {
+    cmd.exe /c rmdir $jreFolderName
+} else {
+    cmd.exe /c mklink /j $jreFolderName "${javaHome}" | Out-Null
+}
