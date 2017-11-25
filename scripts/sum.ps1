ff5834a6fca9e295c24c444a8dd00414497fc01b
frederic <frederic@mach-17.lan>
2017-11-25 10:38:48 +0100

    Added sum script

---
+++ scripts/sum.ps1
@@ -0,0 +1,10 @@
+﻿# usage: sum -file readme.txt -hash SHA256
+
+ # hash algorithms : MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
+
+ param (
+   [Parameter(Mandatory=$true)][string]$file,
+   [string]$hash = 'SHA1'
+ )
+
+ $(certutil.exe -hashfile "$file" $hash.ToUpper())[1] -replace ' ',''
