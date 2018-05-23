aa52d075cef5f882435c43b3fdff4349e276252c
Frederic Fays <frederic@fays.eu>
2018-05-23 14:30:07 +0200

    H2 database ODBC connection example

---
+++ scripts/h2-odbc-connection.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = "Stop"
+
+$conn = New-Object System.Data.Odbc.OdbcConnection
+$conn.ConnectionString = "Driver={PostgreSQL Unicode(x64)};Server=localhost;Port=5435;Database=~/test;Username=sa;Password=sa;"
+$conn.Open()
+$cmd = $conn.CreateCommand()
+$cmd.CommandText = "SELECT 1 AS id UNION SELECT 2 UNION SELECT 3;"
+$reader = $cmd.ExecuteReader()
+$datatable = New-Object System.Data.DataTable
+try {
+    $datatable.Load($reader)
+} catch {
+    $ex =  $_
+}
+$datatable | Format-Table -Auto 
+$conn.Close()
