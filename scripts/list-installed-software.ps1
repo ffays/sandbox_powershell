477b9e0ac5f5b3798a815b95c1788f5091a5064c
Frederic Fays <frederic@fays.eu>
2019-01-22 11:59:26 +0100

    Added a script to list installed SW

---
+++ scripts/list-installed-software.ps1
@@ -0,0 +1,9 @@
+﻿# List installed software
+
+# wmic product get identifyingnumber,name
+
+Get-ItemProperty  HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* `
+| Select-Object PSChildName, DisplayName, DisplayVersion, Publisher, InstallDate `
+| Sort-Object -Property DisplayName `
+| ConvertTo-Csv -NoTypeInformation -Delimiter "`t" `
+| % {$_ -replace '"',''} 
