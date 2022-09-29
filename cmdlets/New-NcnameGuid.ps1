449892a57c129a1f06fd86c2ca6e059721e53994
Frederic Fays <frederic@fays.eu>
2022-09-29 18:41:47 +0200

    Removed New-NcnameGuid

--- cmdlets/New-NcnameGuid.ps1
+++
@@ -1,4 +0,0 @@
-function New-NcnameGuid {
-	$b = (New-Guid).ToByteArray()
-	New-Object Guid(,(@($b[0],$b[1],$b[2],[Byte]($b[3] -bor 160),$b[4],$b[5],$b[6],$b[7],$b[8],$b[9],$b[10],$b[11],$b[12],$b[13],$b[14],$b[15]) -as [Byte[]]))
-}
