16dea27299ac09a0f843b507ee9c16eb2d2bf25a
Frederic Fays <frederic@fays.eu>
2023-03-10 10:54:36 +0100

    Search-Zip

---
+++ cmdlets/Search-Zip.ps1
@@ -0,0 +1,24 @@
+﻿Add-Type -AssemblyName System.IO.Compression
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function Search-Zip {
+    [cmdletbinding()]
+    param (
+        [parameter(mandatory=$true)]$Path,
+        [parameter(mandatory=$true)]$Regex,
+        [switch]$Recurse,
+        $namespace = $null
+    )
+
+    process {
+        Get-ChildItem -Path:$Path -Recurse:$Recurse | % {
+            $zip = $_
+            ([System.IO.Compression.ZipArchive]([System.IO.Compression.ZipFile]::OpenRead($zip))).Entries | ? {
+                $entry = $_
+                $entry.Name -match $Regex
+            } |  % {
+               $zip.Name + ":" + $_.FullName
+            }
+        }
+    }
+}
